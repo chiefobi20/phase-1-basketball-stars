eec812c02b76cda14acd2f63e04418fd3b264ed3
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-01-22 18:41:58 -0500

    project

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
 fetch('http://localhost:3000/basketballPlayers')
 .then(response => response.json())
-
-//assestment stuff
-// const sport = "Soccer"
-
-function playSport(sport){
-    console.log("It's time to play" + sport)
-}
-
-const myFunc = function(){
-    return 'I am having a blast !'
-}
-
-myFunc()
-
-const num = 9
-
-function newValue(){
-    const anotherNum = 10
-    return num * anotherNum
-}
-
-newValue(90)
-
-const foods = ['Cheeseburger']
-foods.push('Pizza')
-
-foods.forEach((food) => {
-    `I love ${food}`
+.then(basketballData => {
+    basketballData.forEach((player) => {
+        addBasketballPlayerImageToDiv(player)
+    })
 })
 
-const newID = document.querySelector('#title-text')
+function addBasketballPlayerImageToDiv(player){
+    const playerImageElement = document.createElement('img')
+    playerImageElement.src = player.image
+    const nbaPlayersDivElement = document.getElementById('nba-players')
+    nbaPlayersDivElement.appendChild(playerImageElement)
+}
 
-newID.addEventListener('click',() => {
-    console.log("Hello, flatiron!")
-} )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //assestment stuff
+// // const sport = "Soccer"
+
+// function playSport(sport){
+//     console.log("It's time to play" + sport)
+// }
+
+// const myFunc = function(){
+//     return 'I am having a blast !'
+// }
+
+// myFunc()
+
+// const num = 9
+
+// function newValue(){
+//     const anotherNum = 10
+//     return num * anotherNum
+// }
+
+// newValue(90)
+
+// const foods = ['Cheeseburger']
+// foods.push('Pizza')
+
+// foods.forEach((food) => {
+//     `I love ${food}`
+// })
+
+// const newID = document.querySelector('#title-text')
+
+// newID.addEventListener('click',() => {
+//     console.log("Hello, flatiron!")
+// } )
