1d112c3307e0418036f9278b7023e2f9ac7725ab
Ahamefule Onyegbule <aonyeobi@gmail.com>
2025-01-23 18:06:57 -0500

    Completed Project Requirements

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
+const nbaPlayersDivElement = document.getElementById('nba-players')
+const catalogDiv = document.getElementById('ball-catalog')
+const playerDisplayDiv = document.getElementById('player-display')
+const basketballImage = document.getElementsByClassName('display-player')[0]
+basketballImage.addEventListener('mouseover', () => {
+    playerDisplayDiv.style.textAlign = "center"
+    console.log(playerDisplayDiv)
+})
+
+
 fetch('http://localhost:3000/basketballPlayers')
 .then(response => response.json())
 .then(basketballData => {
+    handleDisplay(basketballData[0])
+
     basketballData.forEach((player) => {
         addBasketballPlayerImageToDiv(player)
     })
 })
 
+const handleDisplay = (player) => {
+    console.log(player)
 
+    basketballImage.src = player.image
+
+    const playerName = document.getElementById('name-display')
+    playerName.textContent = player.name
+
+    const playerTeam = document.getElementById('team-display')
+    playerTeam.textContent = player["team(s)"]
+
+    const playerDraftInfo = document.getElementById('draft-display')
+    playerDraftInfo.textContent = player["draft year & pick"]
+
+    const playerJerseyNumber = document.getElementById('number-display')
+    playerJerseyNumber.textContent = player["jersey number"]
+
+}
 
 function addBasketballPlayerImageToDiv(player){
     const playerImageElement = document.createElement('img')
+
     playerImageElement.src = player.image
+    playerImageElement.addEventListener('click',() => {
+
+        handleDisplay(player)
+    })
     const nbaPlayersDivElement = document.getElementById('nba-players')
     nbaPlayersDivElement.appendChild(playerImageElement)
 
@@ -23,13 +57,19 @@ const newPlayerForm = document.getElementById('new-player')
         const newTeamInput = document.getElementById('new_team')
         const newDraftInput = document.getElementById('new_draft')
         const newNumberInput = document.getElementById('jersey_number')
+        const newImageInput = document.getElementById('new_image')
+
 
         const newPlayer = {
-            name: newNameInput.value,
-            team: newTeamInput.value,
-            draft: newDraftInput.value,
-            number: newNumberInput.value
+            "name": newNameInput.value,
+            "team(s)": newTeamInput.value,
+            "draft year & pick": newDraftInput.value,
+            "jersey number": newNumberInput.value,
+            "accolades": "",
+            "bio": "",
+            "image": newImageInput.value
         }
+
         fetch('http://localhost:3000/basketballPlayers',{
             method: 'POST',
             headers: {
@@ -39,17 +79,20 @@ const newPlayerForm = document.getElementById('new-player')
             body:JSON.stringify(newPlayer)
         })
         .then(response => {
+
             if(response.ok){
                 response.json().then(newPlayerData => {
                     addBasketballPlayerImageToDiv(newPlayerData)
+                    newPlayerForm.reset()
                 })
             }
             else{
-                alert("Error: Must Add Player!")
+                alert("Error: Unable to Add Player")
             }
         })
     })
 
-    newPlayerForm.reset()
+
+
 
 
